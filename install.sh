276b239c56af03c9ce0d0317686d65a81dc14349
Spitap <perso@asdrip.fr>
2021-07-17 13:07:47 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ install () {
     cp "./NordIndicator.py" "$HOME""/.local/share/NordIndicator/"
     cp "./src/NordIndicator" "$HOME""/.local/bin/"
     cp "./src/Uninstall.sh" "$HOME""/.local/share/NordIndicator/"
-    sed "s/vpn_on/$HOME\/.local\/share\/NordIndicator\/vpn_on.svg" "./src/NordIndicator.desktop" > "$HOME/.local/share/applications/"
+    sed "s/vpn_on/$HOME\/.local\/share\/NordIndicator\/vpn_on.svg" "./src/NordIndicator.desktop" > "$HOME/.local/share/applications/NordIndicator.desktop"
     if [ -f "$HOME""/.config/NordIndicator/config.py" ]; then
         if [ "$1" = "-q" ];then
             echo "New config file saved as config.py.new"
